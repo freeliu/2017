998461bf95ed709bf1e84fb0c68a73d51f337961
olive <olive@meishakeji.com>
2017-01-10 10:51:44 +0800

    '1'

--- vue-cli-demo/src/pages/App2.vue
+++ vue-cli-demo/src/pages/App2.vue
@@ -5,6 +5,7 @@
         <div>这是从app1传过来的id:--------{{id}}</div>
         <div>这是从app1传过来的age:--------{{$route.query.age}}</div>
         <div>这是从app1传过来的gender:--------{{$route.query.gender|genderName}}</div>
+         <div>这是时间{{now|formatDateTo_yyyy_MM_dd}}</div>
 
     </div>
 </template>
@@ -16,11 +17,13 @@
         data(){
             return {
                 random: Math.random(),
-                id:this.$route.query.id
+                id:this.$route.query.id,
+                now:null
             }
         },
         created(){
           console.log(1);
+          this.now=Date.now();
         },
         mounted(){
              this.showMsg("http://cn.vuejs.org/v2/api/#选项-生命周期钩子")
